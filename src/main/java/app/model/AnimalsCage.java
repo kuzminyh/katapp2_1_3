4324d568167d41e53f879f6d50d38bf96524a96b
Кузьминых Сергей <kuzminyhsa@gmail.com>
2024-10-10 14:31:48 +0800

    Equal time

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -12,13 +12,14 @@ public class AnimalsCage {
     @Qualifier("dog")
     private Animal animal;
     @Autowired
+    @Qualifier("timer")
     private Timer timer;
 
     public void whatAnimalSay() {
         System.out.println("Say:");
         System.out.println(animal.toString());
         System.out.println("At:");
-        System.out.println(new Timer().getTime());
+        System.out.println(getTimer().getTime());
         System.out.println("________________________");
     }
 
